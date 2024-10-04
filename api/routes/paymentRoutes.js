d0219e24303cf7f0c89bba87bcd3295ca6d24bac
rifathasan <mdmehedi1111bd@gmail.com>
2024-10-04 20:35:15 +0600

    added all commit

--- api/routes/paymentRoutes.js
+++ api/routes/paymentRoutes.js
@@ -1,29 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const  Payments = require('../models/Payments')
-const ObjectId = mongoose.Types.ObjectId;
-
+const mongoose = require("mongoose");
+const Payments = require("../models/Payments");
 // token
-// const verifyToken = require('../middlewares/verifyToken');
-// const Carts = require("../models/Carts");
+const verifyToken = require("../middlewares/verifyToken");
+const Carts = require("../models/Carts");
+// const ObjectId = mongoose.Types.ObjectId;
+
+router.post("/", verifyToken, async (req, res) => {
+  const payment = req.body;
+//   console.log("Body", payment);
+  try {
+    const paymentRequest = await Payments.create(payment);
+
+    // console.log("PaymentRequest", paymentRequest);
+
+    // delete cart after payment
+    const cartsId = payment.cartItems;
+    console.log("CartId: ", cartsId);
+
+    const deletedCartRequest = await Carts.deleteMany({
+      _id: { $in: cartsId },
+    });
+
+    res.status(200).json({ paymentRequest, deletedCartRequest });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
 
 
-
-router.post('/', async(req,res) => {
-    const payData  = req.body;
-    try {
-        const paymentRequest = await Payments.create(payData)
-
-        // delete cart after payment
-        // const cartsId = payData.cartItems.map( id => ObjectId(id));
-        // const deletedCartRequest = await Carts.deleteMany({_id: {$in:cartsId} })
-       
-        res.status(200).json(paymentRequest)
-
-    } catch (error) {
-        res.status(404).json({message:error.message})
+// get traking order data
+router.get('/', verifyToken, async (req,res) => {
+  const email = req.query.email;
+  const query = {email:email}
+  try {
+    const decodedEmail = req.decoded.email
+    if(email !== decodedEmail){
+      res.status(403).json({message:"forbiden access"})
     }
+    const result = await Payments.find(query).sort({createdAt:-1}).exec();
+    res.status(200).json(result)
+
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 })
 
 
